fdf9f23c8ace43673cd955d1afe04a5c9202da75
JhonyAlex <39017484+JhonyAlex@users.noreply.github.com>
2025-06-20 15:06:39 +0000

    otra vez

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
-import vue from 'vite-plugin-vue2';
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
